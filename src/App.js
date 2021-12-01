e1fd3a91537b0a333daac6ba5ffa81d983213636
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-01 21:44:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import InfinityStones from './Components/InfinityStones';
 function App() {
   return (
     <div className="App">
-      <p>MARIN JE SUPER</p>
-      <p>OVO JE MOJ DRUGI PR</p>
       <InfinityStones>
         
       </InfinityStones>
