8de73792f5b5529e6010f5c2242bd1950715f53b
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-01 21:36:42 +0100

    marinov pr je uspio

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import InfinityStones from './Components/InfinityStones';
 function App() {
   return (
     <div className="App">
+      <p>MARIN JE SUPER</p>
       <InfinityStones>
         
       </InfinityStones>
