7b911de2e4215e81adc130f06816637ec090790e
zrvnarmarin <76marin76@gmail.com>
2021-12-01 22:26:11 +0100

    CSS added for stones

--- src/Components/InfinityStones.jsx
+++ src/Components/InfinityStones.jsx
@@ -15,15 +15,25 @@ const InfinityStones = () => {
             </h1>
             <div className="infinity-stone-section">
                 <div className="stone-picture-row">
-                    <img src={require(`../Pictures/InfinityStones/time_stone.jpg`).default} className="stone-picture"/>
-                    <img src={require(`../Pictures/InfinityStones/mind_stone.jpg`).default} className="stone-picture"/>
-                    <img src={require(`../Pictures/InfinityStones/space_stone.jpg`).default} className="stone-picture"/>
+                    <div className="stone-component">
+                        <img src={require('../Pictures/InfinityStones/time_stone.jpg').default} className="stone-picture"/>
+                        <h3 className="stone-name">{stoneNames[0].stoneName}</h3>
+                    </div>
+                    <div className="stone-component">
+                        <img src={require('../Pictures/InfinityStones/time_stone.jpg').default} className="stone-picture"/>
+                        <h3 className="stone-name">{stoneNames[1].stoneName}</h3>
+                    </div>
+                    <div className="stone-component">
+                        <img src={require('../Pictures/InfinityStones/time_stone.jpg').default} className="stone-picture"/>
+                        <h3 className="stone-name">{stoneNames[2].stoneName}</h3>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas mollitia perferendis rerum
+                             architecto fuga, minus harum nulla commodi repellendus asperiores neque perspiciatis quam, 
+                             laudantium est expedita dolore, velit recusandae similique?
+                        </p>
+                    </div>
                 </div>
-                <div className="stone-picture-row">
-                    <img src={require(`../Pictures/InfinityStones/soul_stone.jpg`).default} className="stone-picture"/>
-                    <img src={require(`../Pictures/InfinityStones/reality_stone.jpg`).default} className="stone-picture"/>
-                    <img src={require(`../Pictures/InfinityStones/power_stone.jpg`).default} className="stone-picture"/>
-                </div>
+
             </div>
         </div>
     )
