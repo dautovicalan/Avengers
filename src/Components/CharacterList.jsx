34caef37683d95ccff485deac23c276f0d4cf045
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-02 20:59:29 +0100

    deleting carosel

--- src/Components/CharacterList.jsx
+++ src/Components/CharacterList.jsx
@@ -1,4 +1,4 @@
-import { Collapse, Button } from 'react-bootstrap'
+import { Collapse, Button, Carousel } from 'react-bootstrap'
 import { useState } from 'react';
 import '../Styles/CharacterList.css'
 
@@ -7,22 +7,28 @@ const CharacterList = () => {
     const[open, setOpen] = useState(false);
 
     return (
-        <div className='character-list'>
-            <Button
-                onClick={() => setOpen(!open)}
-                aria-controls="example-collapse-text"
-                aria-expanded={open}
-            >
-                click
-            </Button>
-            <Collapse in={open}>
-                <div className="character-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Dolore consectetur iusto facere debitis laboriosam atque, 
-                    quo aut dolorem eius aliquid, illum maxime tempora porro 
-                    soluta quae sed alias at impedit.
+        <div className='character-list'> 
+            <div className='single-character'>
+                <div className="character-image">
+                    <img src={require('../Pictures/Characters/thor.jpg').default} alt="" />
                 </div>
-            </Collapse>
+                <h3>Thor</h3>
+                <Button
+                    onClick={() => setOpen(!open)}
+                    aria-controls="example-collapse-text"
+                    aria-expanded={open}
+                >
+                    Show More Information
+                </Button>
+                <Collapse in={open}>
+                    <div className="character-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Dolore consectetur iusto facere debitis laboriosam atque,
+                        quo aut dolorem eius aliquid, illum maxime tempora porro
+                        soluta quae sed alias at impedit.
+                    </div>
+                </Collapse>
+            </div>
         </div>
     )
 }
