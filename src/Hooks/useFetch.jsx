751add35de5af033f15c4e9504229cdb3e7bf45e
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-08 20:06:04 +0100

    fethcing data

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -8,16 +8,16 @@ const useFetch = (resource, characterList) => {
     const[data, setData] = useState([]);
 
     useEffect(() => {
-
         const fetchData = async () => {
             try {
-                let fillingArrary = [];
+                let fillingArrary = []
                 characterList.map(async (element) => {
-                    const response = await characterApi.get(`https://gateway.marvel.com:443/v1/public/${resource}?name=${element}&ts=1&apikey=d2983b072b2fbf494550a767d5b40bfb&hash=d1d17fa9c4005a5d24bfb2a9f6449b78`);
-                    fillingArrary.push(response.data);
-                    console.log(response.data);
-                })
+                    const response = await characterApi.get(`https://gateway.marvel.com:443/v1/public/${resource}?name=${element}&ts=1&apikey=${apiKey}&hash=d1d17fa9c4005a5d24bfb2a9f6449b78`);
+                    fillingArrary.push(response.data.data.results[0]);
+                    console.log(data);
+                });
                 setData(fillingArrary);
+                console.log(data);
             } catch (error) {
                 console.log(error.message);
             }
--- src/Pages/CharacterPage.jsx
+++ src/Pages/CharacterPage.jsx
@@ -5,20 +5,20 @@ import '../Styles/CharacterPage.css'
 const CharacterPage = () => {
 
     const characterNames = [encodeURIComponent("iron man"), encodeURIComponent("thor"), encodeURIComponent("captain america"),
-                            encodeURIComponent("thanos"), encodeURIComponent("spider-man"), encodeURIComponent("black widow"),
-                            encodeURIComponent("black panther"), encodeURIComponent("antman")];
+                            encodeURIComponent("thanos"), encodeURIComponent("hulk"), encodeURIComponent("black widow"),
+                            encodeURIComponent("black panther"), encodeURIComponent("Doctor Strange")];
 
     const charactersInfo = useFetch('characters', characterNames);
-    console.log(charactersInfo);
 
+    console.log(charactersInfo.length);
     return (
         <div className="characters-container" style={{marginTop: "100px"}}>
             {charactersInfo.length != 0 && charactersInfo.map((element) =>{
                 return(
                     <div className="single-character" key={element.id}>
                         <img src={element.picture} height="200px" width="200px"/>
-                        <h1>{element.characterName}</h1>
-                        <p>{element.characterLore}</p>
+                        <h1>{element.name}</h1>
+                        <p>{element.description}</p>
                     </div>
                 )
             })}
