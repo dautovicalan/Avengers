052904502da78715037327a832249c0f75d4f292
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-08 22:22:48 +0100

    animations

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -20,7 +20,6 @@ const useFetch = (resource) => {
             console.log(error.message);
         }
     }
-
     useEffect(() => {
         fetchData();
     }, []);
--- src/Pages/CharacterPage.jsx
+++ src/Pages/CharacterPage.jsx
@@ -8,16 +8,19 @@ const CharacterPage = () => {
     console.log(charactersInfo);
 
     return (
-        <div className="characters-container" style={{marginTop: "100px"}}>
-            {charactersInfo.length != 0 && charactersInfo.map((element, index) =>{
-                return(
-                    <div className="single-character" key={index}>
-                        <img src={`${element.thumbnail.path}.${element.thumbnail.extension}`} height="200px" width="200px"/>
-                        <h1>{element.name}</h1>
-                        {element.description !== "" ? <p>{element.description}</p> : <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint accusantium molestiae error. Culpa quasi, nesciunt fugit sed possimus esse molestiae cupiditate tempore iste laborum repellat illum reiciendis modi veritatis omnis.</p>}
-                    </div>
-                )
-            })}
+        <div className="character-component-page">
+            <h1 style={{textAlign: "center", fontSize:"3.4rem", marginTop:"10px"}}>Your Endgame Heroes</h1>
+            <div className="characters-container" style={{marginTop: "100px"}}>
+                {charactersInfo.length != 0 && charactersInfo.map((element, index) =>{
+                    return(
+                        <div className="single-character" key={index}>
+                            <img src={`${element.thumbnail.path}.${element.thumbnail.extension}`} height="200px" width="200px"/>
+                            <h1>{element.name}</h1>
+                            {element.description !== "" ? <p>{element.description}</p> : <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint accusantium molestiae error. Culpa quasi, nesciunt fugit sed possimus esse molestiae cupiditate tempore iste laborum repellat illum reiciendis modi veritatis omnis.</p>}
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
