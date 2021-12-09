c27aa158237abc288c387ce8a2cd0e6ca878c3a3
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-10 00:07:47 +0100

    pulling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ import img4 from '../Pictures/marvi.jpg'
 import '../Styles/FooterStyles.css'
 
 const Footer = () => {
+
     return (
         <footer className="footer-container">
             <img src={img4} alt="Footer" />
