77b7a8328528f3ed3fb22b007be8d4305ad47123
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-10 00:26:21 +0100

    tweaking animations

--- src/Pages/StoryPage.jsx
+++ src/Pages/StoryPage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../Styles/Story.css'
-import AOS from 'aos'
 import 'aos/dist/aos.css'
 import pic1 from '../Pictures/StoryPictures/Samuel_Wilson.png'
 import pic2 from '../Pictures/StoryPictures/aot.png'
@@ -21,27 +20,6 @@ import pic16 from '../Pictures/StoryPictures/funeral.png'
 import pic17 from '../Pictures/StoryPictures/ende.png'
 
 const StoryPage = () => {
-    AOS.init({
-         // Global settings:
-            disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-            startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-            initClassName: 'aos-init', // class applied after initialization
-            animatedClassName: 'aos-animate', // class applied on animation
-            useClassNames: false, // if true, will add content of data-aos as classes on scroll
-            disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-            debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-            throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-
-            // Settings that can be overridden on per-element basis, by data-aos-* attributes:
-            offset: 120, // offset (in px) from the original trigger point
-            delay: 0, // values from 0 to 3000, with step 50ms
-            duration: 2000, // values from 0 to 3000, with step 50ms
-            easing: 'linear', // default easing for AOS animations
-            once: false, // whether animation should happen only once - while scrolling down
-            mirror: false, // whether elements should animate out while scrolling past them
-            anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-    });
     return (
         <div>
             <div className="flexbox-container">
