7256c05162528a363e75a043da227789fb05e1bd
RealMeKaNiK <dautovic2000@gmail.com>
2021-12-09 12:06:30 +0100

    removed flip animation and navbar fix

--- src/Components/CharacterList.jsx
+++ src/Components/CharacterList.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import '../Styles/CharacterList.css'
-import '../Styles/quake.css'
 import { Link } from 'react-router-dom';
 
 const CharacterList = () => {
@@ -10,48 +9,32 @@ const CharacterList = () => {
         <div className='characters-container-list'>
             <div className='character-list'>
                 <Link to={`/character`}>
-                    <div class="flip-box shake-me">
+                    <div class="flip-box">
                         <div class="flip-box-inner">
                             <div class="flip-box-front">
                                 <img src={require('../Pictures/Characters/ironman.jpg').default} alt="Paris" style={{width:"400px", height:"400px"}} />
                             </div>
-                            <div class="flip-box-back" >
-                                <h2>Iron Man</h2>
-                                <p>What an amazing super hero</p>
-                            </div>
                         </div>
                     </div>
                 </Link>
-                <div class="flip-box shake-me">
+                <div class="flip-box">
                     <div class="flip-box-inner">
                         <div class="flip-box-front">
                             <img src={require('../Pictures/Characters/thor.jpg').default} alt="Paris" style={{width:"400px", height:"400px"}} />
                         </div>
-                        <div class="flip-box-back shake-me">
-                            <h2>Thor</h2>
-                            <p>What an amazing super hero</p>
-                        </div>
                     </div>
                 </div>
-                <div class="flip-box shake-me">
+                <div class="flip-box">
                     <div class="flip-box-inner">
                         <div class="flip-box-front">
                             <img src={require('../Pictures/Characters/captainamerica.jpg').default} alt="Paris" style={{width:"400px", height:"400px"}} />
                         </div>
-                        <div class="flip-box-back">
-                            <h2>Captain America</h2>
-                            <p>What an amazing super hero</p>
-                        </div>
                     </div>
                 </div>
-                <div class="flip-box shake-me">
+                <div class="flip-box">
                     <div class="flip-box-inner">
                         <div class="flip-box-front">
                             <img src={require('../Pictures/Characters/thanos.jpg').default} alt="Paris" style={{width:"400px", height:"400px"}} />
-                        </div>
-                        <div class="flip-box-back">
-                            <h2>Thanos</h2>
-                            <p>What an amazing super hero</p>
                         </div>
                     </div>
                 </div>
